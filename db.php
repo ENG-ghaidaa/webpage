c7cbe5799997ee6ef33f34c826529421d89767e0
ENG-ghaidaa <ghaidaahassan20@gmail.com>
2026-08-02 23:41:52 +0300

    Add database connection script in db.php

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+// Database connection settings
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "person_database";
+
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $database);
+
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
